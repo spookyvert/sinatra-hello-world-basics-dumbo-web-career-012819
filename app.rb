41e57e9480ec824287a0a0e1a528efe33cf28794
Shemar Dacosta <dshemar6@gmail.com>
2019-02-17 16:13:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do 
+    "Welcome to your app!!!! "
+    "Started my server using Shotgun!"
+  end
 
 
 end
